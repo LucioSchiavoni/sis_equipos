e17f0b2ac7388a8aa8b472cbae7f930cda1ca8ed
Lucio B. Schiavoni Lafourcade <lucio.schiavoni@mec.local>
2024-09-04 12:33:54 -0300

    standalone deploy

--- sis_tasks/next.config.mjs
+++ sis_tasks/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone",
+    output: "node .next/standalone/server.js",
     typescript:{
         ignoreBuildErrors: true,
     }
